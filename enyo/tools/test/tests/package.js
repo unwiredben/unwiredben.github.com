eebc313341eb4ed6034d06e5f58b9463af73a6bb
unwiredben <combee@techwood.org>
2012-02-18 18:52:02 -0600

    add enyo

---
+++ enyo/tools/test/tests/package.js
@@ -0,0 +1,10 @@
+enyo.depends(
+	"ComponentDispatchTest.js",
+	"ComponentHandlersTest.js",
+	"ControlPropsTest.js",
+	"DecodePackagePathTest.js",
+	"langTest.js",
+	"KindTest.js",
+	"AsyncTest.js",
+	"AjaxTest.js"
+);
